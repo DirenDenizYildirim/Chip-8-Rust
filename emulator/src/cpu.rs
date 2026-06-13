13a615705f36f524d28a99535cdec1e0c9390343
DirenDenizYildirim <direndenizyildirim2008@gmail.com>
2026-06-13 20:28:41 +0300

    finilized the struct

--- emulator/src/cpu.rs
+++ emulator/src/cpu.rs
@@ -3,8 +3,9 @@ pub struct Chip8 {
     registers: [u8; 16],
     index: u16,
     pc: u16,
-    // stack goes here
-    delay: u8, // they might be in the integer type but I aint sure
+    stack: [u16; 16],
+    sp: usize,
+    delay: u8,
     sound: u8,
     display: [[bool; 64]; 32],
     keypad: [bool; 16],
